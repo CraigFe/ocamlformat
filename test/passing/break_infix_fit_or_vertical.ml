c7966ac8aac1d0b17d77ca27a16dab42aad0e82a
Craig Ferguson <me@craigfe.io>
2020-12-02 17:58:37 +0100

    Add a reproduction case for issue #1094

---
+++ test/passing/break_infix_fit_or_vertical.ml
@@ -0,0 +1,7 @@
+let () =
+  aaaaaaaaaaaaaaaaaaaaaaaaaaaa |> bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb |> cccccc
+  |> fun x -> ddddddddddd
+
+let () =
+  aaaaaaaaaaaaaaaaaaaaaaaaaaaa |> bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb |> cccccc
+  |> function x -> ddddddddddd
